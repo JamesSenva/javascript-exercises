404be1d4216e9b08391ca6f01134cf6dcbfe52ce
James Senva <jamessenva@gmail.com>
2023-12-02 23:54:32 +0530

    Remove other arguments using filter and forEach methods

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    
+    // first method using filter method
+    // return arr.filter( ele => !args.includes(ele) );
 
+    // second method using forEach method
+    const newArr = [];
+    arr.forEach( ele => {
+        if(args.includes(ele)){
+            newArr.push(ele);
+        }
+    })
+    return newArr;
 };
 
 // Do not edit below this line
