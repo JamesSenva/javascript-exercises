5662e1fb0df103efd813c645561ff23e0292490c
James Senva <jamessenva@gmail.com>
2024-01-22 23:07:29 +0530

    Finish all the tasks and also add shorter versions with arrow functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,63 @@
-const add = function() {
-	
+// const add = function(a, b) {
+//   	return a + b;
+// };
+const add = (a, b) => a + b;
+
+
+
+// const subtract = function(a, b) {
+// 	return a - b;
+// };
+const subtract = (a, b) => a - b;
+
+
+
+//----------- sum exercise
+
+// const sum = function(arr) {
+// 	// return arr.reduce( (total, cur) => {
+// 	// 	return total += cur;
+// 	// }, 0)
+// 	return arr.reduce( (total, cur) => (total += cur), 0);
+// };
+
+// whole code block above can be written like below using arrow function
+const sum = arr => arr.reduce( (total, cur) => (total += cur), 0);
+//----------- sum exercise
+
+
+
+//----------- multiply exercise
+
+// const multiply = function(arr) {
+// 	return arr.reduce( (sum, cur) => {
+// 		return sum * cur;
+// 	})
+// };
+
+// shorter way with arrow functions
+const multiply = arr => arr.reduce( (sum, cur) => (sum *= cur));
+
+//----------- multiply exercise
+
+
+
+const power = function(n1, n2) {
+	return Math.pow(n1,n2);
 };
 
-const subtract = function() {
-	
-};
 
-const sum = function() {
-	
-};
 
-const multiply = function() {
+const factorial = function(n) {
+	let numbers = [];
+	for(let i = n; i >= 1; i--){
+			numbers.push(i);
+	}
 
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+	// no need for these two lines below, just return it
+	// const total = numbers.reduce( (acc, cur) => (acc * cur), 1);
+	// return total;
+	return numbers.reduce( (acc, cur) => (acc * cur), 1);
 };
 
 // Do not edit below this line
