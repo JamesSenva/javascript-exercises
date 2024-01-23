f83cd5e41932c5db4e6bacaa3d78153ab2aa09f9
James Senva <jamessenva@gmail.com>
2024-01-23 21:28:37 +0530

    Check if the string is palindrome or not

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const originalStr = str.replace(/[\.,?!\s]/g, "").toLowerCase();
+	const reversedStr = str.split('').reverse().join('').replace(/[\.,?!\s]/g, "").toLowerCase();
+    return (originalStr === reversedStr);
 };
 
 // Do not edit below this line
