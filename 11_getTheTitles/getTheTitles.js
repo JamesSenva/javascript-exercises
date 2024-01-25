4fc1276d9ce5148dffe2db19baf33f40e16b81d8
James Senva <jamessenva@gmail.com>
2024-01-25 23:12:27 +0530

    Get the titles of the books

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,17 @@
-const getTheTitles = function() {
+const getTheTitles = function(arr) {
 
+    // first method
+    // const titles = [];
+    
+    // for (const item of arr)  {
+    //     titles.push(item.title);
+    // }
+    
+    // second method
+    const titles = arr.map(book => book.title);
+
+    
+    return titles;
 };
 
 // Do not edit below this line
